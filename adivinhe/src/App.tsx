724c4539876d94e7bca8e4b9565ee7cc40d0dcf1
Luiz G Cavagna <luizcavagna@gmail.com>
2026-06-11 15:56:00 -0300

    component Input

--- adivinhe/src/App.tsx
+++ adivinhe/src/App.tsx
@@ -2,6 +2,8 @@ import styles from "./app.module.css";
 
 import { Tip } from "./components/Tip";
 import { Header } from "./components/Header";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
 
 export default function App() {
 
@@ -12,8 +14,24 @@ export default function App() {
   return (
     <div className={ styles.container }>
       <main>
+        
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        
         <Tip tip="Uma das linguagens de programação mais utilizadas no mundo" />
+        
+        <div className={styles.word}>
+          <Letter value="R"/>
+          <Letter value="E"/>
+          <Letter value="A"/>
+          <Letter value="C"/>
+          <Letter value="T"/>
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
+        </div>
       </main>
     </div>
   );
